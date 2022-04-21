d992e14c9bf2339aaf6612561ea2c473eed72f39
Henry Miller <mail.henrym+git@gmail.com>
2022-04-21 11:34:06 -0400

    finished without optimization

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -5,4 +5,21 @@
 
 def first_spicy_batch(n)
   # your code here
+
+  # given an array of ints of nth lenght, iterate through the array to find the first set that returns true
+  # 1. map through the array
+  # 2. run through the value of the current index through is_spicy_batch
+  # 3. if the value returns true, return the index
+  # 4. if not, continue 
+
+  # optimizing:
+  # use ruby method to find first index where the provided condition returns true 
+
+  (1..n).detect{|i| is_spicy_batch(i) ==true} 
+
+  #  for i in (1..n)
+  #   if is_spicy_batch(i) == true
+  #      i
+  #   end
+  # end
 end
